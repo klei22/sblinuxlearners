9e79a5cd2e2a0499581200d54ca8bd97baf05c55
klei22 <kauna@avalonwest.tech>
2024-07-18 15:41:05 -0700

    Add install script for workshop dependencies

---
+++ install_deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install firefox
+sudo add-apt-repository ppa:mozillateam/ppa
+sudo apt update
+sudo apt install firefox
+
+# install python dependencies
+sudo apt install python3-virtualenv
+sudo apt install python3-pip
