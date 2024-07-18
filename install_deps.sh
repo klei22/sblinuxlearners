2e148dff17f1f0b3e25ddcbd28b242353f5cf490
klei22 <kauna@avalonwest.tech>
2024-07-18 16:49:03 -0700

    Readd firefox via snaps

--- install_deps.sh
+++ install_deps.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # install firefox
-sudo add-apt-repository ppa:mozillateam/ppa
-sudo apt update
-sudo apt install -y firefox
+sudo apt install firefox
 
 # install python dependencies
+sudo apt update
 sudo apt install -y python3-virtualenv
 sudo apt install -y python3-pip
