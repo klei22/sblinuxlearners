6d6e3d70c4f221600986f25d227636b5341986f3
klei22 <kauna@avalonwest.tech>
2024-07-18 17:21:57 -0700

    add dpkg install method for firefox

--- install_deps.sh
+++ install_deps.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # install firefox
-sudo apt install firefox
+# sudo apt install firefox
+wget https://ftp.mozilla.org/pub/firefox/releases/128.0/linux-x86_64/en-US/firefox-128.0.deb
 
 # install python dependencies
-sudo apt update
-sudo apt install -y python3-virtualenv
-sudo apt install -y python3-pip
+# sudo apt update
+# sudo apt install -y python3-virtualenv
+# sudo apt install -y python3-pip
