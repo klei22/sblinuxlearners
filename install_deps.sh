0bc00413b2726e30c4e16402826d4cb8c679be04
klei22 <kauna@avalonwest.tech>
2024-07-18 17:34:59 -0700

    Add programmatic method for installing firefox

--- install_deps.sh
+++ install_deps.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+# install git
+echo "installing git"
+suod apt install -y git
+
+# install vim
+echo "installing vim"
+suod apt install -y vim
+
 # install firefox
-# sudo apt install firefox
-wget https://ftp.mozilla.org/pub/firefox/releases/128.0/linux-x86_64/en-US/firefox-128.0.deb
+echo "installing firefox"
+wget https://ftp.mozilla.org/pub/firefox/releases/128.0/linux-x86_64/en-US/firefox-128.0.tar.bz2
+tar xjf firefox-128.0.tar.bz2
+mkdir ~/Apps
+mv ./firefox ~/Apps/firefox
+sudo ln -s ~/Apps/firefox/firefox /usr/bin/firefox
 
 # install python dependencies
-# sudo apt update
-# sudo apt install -y python3-virtualenv
-# sudo apt install -y python3-pip
+echo "installing python"
+sudo apt update
+sudo apt install -y python3-virtualenv
+sudo apt install -y python3-pip
